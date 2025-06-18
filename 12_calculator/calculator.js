404405e6088cc05c8c6cf568c3e5e4b7d147e94a
dandfres <168474016+dandfres@users.noreply.github.com>
2025-06-17 22:31:04 -0500

    Exercise /12_calculator complete

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((result, e) => result + e, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, f) => product * f, 1);
 };
 
-const power = function() {
-	
+const power = function(x, n) {
+  return x ** n;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let f = 1
+  if (n >= 0) {
+    for (let i = 1; i <= n; i++) {
+      f *= i
+    };
+    return f
+  }
 };
 
 // Do not edit below this line
