7414bea6fa23aa6fd3cfa6b066cfc4051ae171ac
dandfres <168474016+dandfres@users.noreply.github.com>
2025-06-18 21:30:19 -0500

    Exercise 14_fibonacci complete

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    const succesion = [0, 1];
+    if (n < 0) {
+        return 'OOPS';
+    } else {
+        for (let i = 1; i < n; i++) {
+            succesion.push(succesion[i - 1] + succesion[i])
+        }
+        return succesion[n]
+    }
 };
 
 // Do not edit below this line
