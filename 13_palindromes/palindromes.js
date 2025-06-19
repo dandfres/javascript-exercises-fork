a38647a4e63c8e54a8b4f3bd5f60f3faeb54e749
dandfres <168474016+dandfres@users.noreply.github.com>
2025-06-18 20:28:32 -0500

    Exercise 13_palindromes complete

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    const cleanedString = word
+        .toLowerCase()
+        .split('')
+        .filter(char => alphabet.includes(char))
+        .join('');
+    const reverseString = cleanedString.split('').reverse().join('');
+    return cleanedString === reverseString;
 };
 
 // Do not edit below this line
